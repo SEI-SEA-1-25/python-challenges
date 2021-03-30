a42d17ce88ce01a31c2f59c6d67261baa441f8b6
CHRISDAVIDSPICER <CHRISDAVIDSPICER@GMAIL.COM>
2021-03-30 08:22:19 -0700

    Chris Spicer Python Deliverable

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,34 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
+def dep_function():
+  print('How much would you like to deposit?')
+  dep = input(prompt)
+  dep = int(dep)
+  new_balance = current_balance + dep
+  return new_balance
 
+def draw_function():
+  print('How much would you like to withdraw?')
+  draw = input(prompt)
+  draw = int(draw)
+  new_balance = current_balance - draw
+  return new_balance
+
+prompt = '>'
+current_balance = 4000
+new_balance = 0
+
+print(f'Your current balance is {current_balance}')
+print('What would you like to do? (deposit, withdraw, check_balance)')
+task = input(prompt)
+if task in ['deposit', 'withdraw', 'check_balance']:
+  pass
+else:
+  print('Not a valid option')
+if task == 'deposit':
+  dep_function()
+elif task == 'withdraw':
+  draw_function()
+elif task == 'check_balance':
+  pass
+print(f'Your current balance is {current_balance}')
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,12 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+string = 'supercalifragilisticexpialidocious'
+new_list = list(string)
+# print(dir(string))
+new_list = sorted(new_list)
+
+new_string = ''.join(new_list)
+
+print(new_string)
