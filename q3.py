c2c3e6a6c0271865bc9ece18eb24e8e61c66ab2b
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-30 10:18:19 -0700

    more notes in q3

--- q3.py
+++ q3.py
@@ -1,18 +1,25 @@
+# init balance var
 balance = 50000000
 print("💲 Welcome to Gringott's Bank 💲")
 
+# if ask for deposit, enter here
 def deposit():
   return float(input("Deposit amount?"))
 
+# if ask for withdraw, enter here
 def withdraw():
   return float(input("Withdraw amount?"))
 
+# if ask for balance, tell current balance
 def print_balance():
-  print(f"Balance is: {balance}")
+  print(f"Balance: {balance}")
 
+# keeps loop going:
 print_balance()
 is_finished = "no"
 
+# while loop. while user doesn't close loop
+# ask what action they'd like
 while is_finished != "yes":
   command = input("Deposit, withdraw, or balance?")
   if command == "deposit":
@@ -21,6 +28,8 @@ while is_finished != "yes":
     balance -= withdraw()
 
   print_balance()
+  # input ASKS THE USER to define the what it equates to.
   is_finished = input("Are you finished?")
 
-print("Have a nice day!")
+#goodbye message
+print("Have a nice day!🍎 🍎 🍎 ")
