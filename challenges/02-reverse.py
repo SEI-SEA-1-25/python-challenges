40c58f17556f124bc3a02a247301291debbb8a05
wonkatin <j.drawers@gmail.com>
2021-03-30 11:57:49 -0500

    homework

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,22 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+# def reverse_string():
+#     inputsomething = input("what message would you like to reverse?")
+#     x = ''
+#     for letter in inputsomething:
+#         x = letter + x
+#     print(x)
+
+# reverse_string()
+
+# colin's code along
+user_input = input("what message would you like to reverse?")
+def reverse(s):
+    result = ''
+    for letter in s:
+        result = letter + result
+
+    return result
+print(f'{user_input} reversed is: {reverse(user_input)}')
