23eb1d7ccad9dcfb3da7c3594baeb964120f644b
soobinkiki <j.sb.parkk@gmail.com>
2021-03-29 22:31:25 -0700

    challenges done

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,58 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+# Challenge 3 - Bank Transactions
+# Create a prompt that asks the user if they would like to display their balance, 
+# withdraw or deposit. Write three methods to perform these calculations and output the result to the user.
 
+# Gather user input using the input function. Note that input always returns user input as a string. 
+# You have to manually convert it to an int or a float to make it behave like number. 
+# Also, end the input prompt with a \n newline character if you want the user to type in on the next line.
+
+# age = input("How old are you?\n")
+# age = int(age)
+
+# Here is a sample output:
+
+# Your current balance is
+# 4000
+# What would you like to do? (deposit, withdraw, check_balance)
+# deposit
+# How much would you like to deposit?
+# 1000
+# Your current balance is 5000
+# Are you done?
+# yes
+# Thank you!
+
+user_choice = ["deposit", "withdraw", "check_balance"]
+balance = 0
+balance = int(balance)
+
+age = input("How old are you?\n")
+age = int(age)
+
+# age < 18 -> return True
+# else -> return false
+def is_user_age_valid(age):
+    # print(age)
+    if age < 18:
+        print("You must be 18 or older to continue Sorry\n")
+        return False
+    else:
+        return True
+
+returned = is_user_age_valid(age)       
+if returned == True:
+    options = input("What would you like to do? (deposit, withdraw, check_balance)\n")
+    if options not in user_choice:
+        print("Please choose valid options")
+    elif options == "deposit":
+        user_deposit = input("How much would you like to deposit?\n")
+        user_deposit = int(user_deposit)
+        balance += user_deposit
+        print(f"Your balance after deposit: {balance}")
+    elif options == "withdraw":
+        user_withdraw = input("How much would you like to withdraw?\n")
+        user_withdraw = int(user_withdraw)
+        balance -= user_withdraw
+        print(f"Your balance after withdraw: {balance}")
+    else:
+        print(f"This is your current balance: {balance}")
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,20 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+# Create a function that takes a string and returns the string with the letters in alphabetical order 
+# (ie. hello becomes ehllo), Assume numbers and punctuation symbols will not be included in the string.
+
+# Give me a string to alphabetize
+# supercalifragilisticexpialidocious
+# Alphabetized: aaacccdeefgiiiiiiillloopprrssstuux
+
+
+get_string = input("Please enter string: ")
+
+def random_string (value):
+    one_word = sorted(value)
+    print(one_word)
+    join_value = ''.join(one_word)
+    print(join_value)
+random_string(get_string)
