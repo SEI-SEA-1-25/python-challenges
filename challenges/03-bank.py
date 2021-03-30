6f3c0f42f45b9e9ca681b8f90aa0a8f1f91c6a5e
henry-mcdonald <a.henrymcdonald.a@gmail.com>
2021-03-30 13:47:45 -0500

    henry hw

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,32 @@
 print("Welcome to Chase bank.")
+
+starting_balance = 4000
+print(f"Your current balance is {starting_balance}")
+
+
+
+
+loop_condition = True
+while(loop_condition):
+    action = input("What would you like to do? (deposit, withdraw, check_balance)")
+    if(action =="deposit"):
+        value = input("How much would you like to deposit?")
+        starting_balance += int(value)
+        print("Your current balance is "+ str(starting_balance))        
+    elif(action =="withdraw"):
+        value = input("How much would you like to deposit?")
+        starting_balance -= int(value)
+        print("Your current balance is "+ str(starting_balance))  
+    elif(action =="check_balance"):
+        print("Your current balance is "+ str(starting_balance))  
+    else:
+        print("that is not an option")
+    
+    done_condition = input("Are you done?")
+
+    if(done_condition in ["yes","y", "Yes", "Y","YES"]):
+        loop_condition = False
+
+
 print("Have a nice day!")
 
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,17 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+stringToAlphabetize = input("Give me a string to alphabetize")
+
+output = ""
+for letter1 in alphabet:
+    for letter2 in stringToAlphabetize:
+        if(letter1 == letter2):
+            output += letter1
+
+print(output)
