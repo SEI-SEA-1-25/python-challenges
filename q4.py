b0dca7c97471433d8c32c8a015d443dd0164253c
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-30 10:01:20 -0700

    added notes

--- q4.py
+++ q4.py
@@ -1,6 +1,10 @@
+## asks user to enter a string
 string = input("Enter a string: ")
+##turns string entered into a list of letters
 characters = list(string)
+## sorts the individual letters alphabetically 
 characters.sort()
+## joins the letters back into a string
 result = ''.join(characters)
 
 print(f"A: {result}")
