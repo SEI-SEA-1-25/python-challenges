511a0293dd0ca0f66802f5ab8c380b555e64a8ad
NBryant83 <nakisha.bryant@gmail.com>
2021-03-30 14:58:44 -0400

    hw commit

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,21 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+operation = input("What calculation would you like to do?")
+user_input2 = input("What is number 1?")
+user_input3 = input("What is number 2?")
+number1 = int(user_input2)
+number2 = int(user_input3)
+# print(number1 / number2)
+
+if operation == "add":
+  print(f"Your result is {number1 + number2}")
+elif operation == "sub":
+  print(f"your result is {number1 - number2}")
+elif operation == "mult":
+  print(f"your result is {number1 * number2}")
+elif operation == "div":
+  print(f"your result is {number1 / number2}")
+else:
+  print("That was not a valid operation. You can type in 'add' or 'sub' or 'mult' or 'div' ")
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,37 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+print("Welcome to Chase bank. We suck!")
 
+balance = 4000
+
+# print out their balance
+# ask them what they want to do
+# do that thing
+# as long as they don't say they're done...
+# ... keep asking and doing
+
+def print_balance():
+  print(f"Your balance is:\n{balance}")
+
+def deposit():
+  user_input = input("How much would you like to deposit?\n")
+
+  return int(user_input)
+def withdrawal():
+  user_input = input("How much would you like to withdrawal?\n")
+
+  return int(user_input)
+
+keep_going = "y"
+
+print_balance()
+
+while keep_going == "y":
+  command = input("\nWhat would you like to do? (deposit, withdrawal, balance)\n")
+  if command == "deposit":
+    balance += deposit()
+  elif command == "withdrawal":
+    balance -= withdrawal()
+  
+  print_balance()
+  keep_going = input(f"Do you want another transaction? (y/n)")
+
+print("Have a nice day!")
