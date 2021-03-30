c1db73b6b690caac6b77727da51a7c3ab4667a57
ChrisB007 <chrisbitoy@gmail.com>
2021-03-30 11:04:09 -0400

    python challenges

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,32 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+todo = input('what would you like to do today - add, subtract, divide, multiply')
+
+#Options
+responseOne = input('Enter first number')
+responseTwo = input('Enter second number')
+
+# Converts to numbers
+numberOne = int(responseOne)
+numberTwo = int(responseTwo)
+
+
+
+# Calc
+
+def calculate(num1, num2):
+    if add:
+        print(numberOne + numberTwo)
+    elif subtract:
+        print(numberOne - numberTwo)
+    elif multiply:
+        print(numberOne - numberTwo)
+    elif divide:
+        print(numberOne / numberTwo)
+    else:
+        print('Enter a number')
+
+
+
