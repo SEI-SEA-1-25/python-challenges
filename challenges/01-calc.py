f831e2f8803abf3bf415c25a32dacfdeb83eab0d
Jcom619 <joshuacompton@my.smccd.edu>
2021-03-31 01:42:14 -0700

    jc

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -1,4 +1,22 @@
-# Use the `input()` function to prompt a user to enter something.
-# input() always returns a string value. If you ever want someone
-# to enter a number you have to use the `int()` function to convert
-# what they typed in to a string.
+
+
+# a_value = input(int)
+# print('Enter a value:')
+# if type converted_num(str):
+#     print('')
+# else print('wrong!!!')
+
+
+# print('Hello, ' + converted_num)
+# print(type(a_value))
+# print(type(converted_num))
+
+num1 = input("give num1 a value: ")
+num2 = input("give num2 a value: ")
+
+add: print(int(num1) + int(num2))
+subtract: print(int(num1) - int(num2))
+divide: print(int(num1) // int(num2))
+multiply: print(int(num1) * int(num2))
+
+user_input = input("options add, subtract, multiply, divide \ncalc type? ")
--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,31 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+# Python code to convert string to list character-wise
+# # todo #####################
+# # todo: text to string
+# txt_to_list = "¡Hola Mundo!"
+
+
+# def converted_txt(txt_to_list):
+#     # todo: CONVERTS
+#     list_of_txt = []
+#     list_of_txt[:0] = "string"
+#     return list_of_txt
+
+
+# # todo: DEFINES WHAT TO CONVRT
+# # todo: SHOWS CONVRSION
+# print(converted_txt)
+# # ###################################### todo
+# print(txt_to_list)
+# print(converted_txt(txt_to_list))
+# ############################################
+# r_list = converted_txt(txt_to_list).reverse()
+# print(r_list)
+
+# def copy(s)
+
+
+user_input = input("reverse what?")
